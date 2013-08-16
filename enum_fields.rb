58b1a2a07ddd7742ed6902821993be42dae5295a
Erik Wogstad <erik@downhomesolutions.com>
2013-08-15 17:41:22 -0700

    done ch5

---
+++ enum_fields.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby 
+
+ActiveRecord::Base.connection.tables.each do |table_name|
+  puts "\n" + table_name
+  ActiveRecord::Base.connection.columns(table_name).each {|c| puts "- " + c.name + ": " + c.type.to_s + ": " +  c.limit.to_s}
+end
