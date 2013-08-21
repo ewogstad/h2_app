29315c163ae7600be323516267cae526cca988d2
Erik Wogstad <erik@downhomesolutions.com>
2013-08-21 11:03:09 -0700

    done with ch8

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,13 +34,18 @@ class User < ActiveRecord::Base
     encrypted_password == encrypt(submitted_password)
   end
 
+#  def self.authenticate(email, submitted_password)
+#    user = find_by_email(email)
+#    return nil  if user.nil?
+#    return user if user.has_password?(submitted_password)
+#  end
+
   def self.authenticate(email, submitted_password)
     user = find_by_email(email)
-    return nil  if user.nil?
-    return user if user.has_password?(submitted_password)
+    user && user.has_password?(submitted_password) ? user: nil
   end
 
-  private
+ private
     
     def encrypt_password
       self.salt = make_salt if new_record?
