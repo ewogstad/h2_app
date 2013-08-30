e8c38832863073220f35e869e6c8533aa064ceb3
Erik Wogstad <erik@downhomesolutions.com>
2013-08-30 09:45:05 -0700

    begin ch8

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -41,21 +41,15 @@ describe "Users" do
     
     describe "failure" do
       it "should not sign a user in" do
-        visit signin_path
-        fill_in :email,      :with => ""
-        fill_in :password,   :with => ""
-        click_button
-        response.should have_selector("div.flash.error", :content => "Invalid")
+        user = User.new(:email => "", :password => "") 
+        integration_sign_in(user)
       end
     end
 
     describe "success" do
       it "should sign a user in and out" do
         user = Factory(:user)
-        visit signin_path
-        fill_in  :email,        :with => user.email
-        fill_in  :password,     :with => user.password
-        click_button
+        integration_sign_in(user)
         controller.should be_signed_in
         click_link "Sign out"
         controller.should_not be_signed_in
